90e34a1e47db128240576c3473b20eedc5e4a16f
Kyle Dagnan <kdagnan@ufl.edu>
2019-09-29 19:31:10 -0400

    Final Version

--- server/config/express.js
+++ server/config/express.js
@@ -53,6 +53,7 @@ module.exports.init = function() {
       If no path segments are passed, path.resolve() will return the absolute path of the current working directory.
    */
    //res.sendFile(path.resolve(...));
+   res.status(301).redirect("/");
   });
   
   return app;
